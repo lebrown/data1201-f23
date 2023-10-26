fe2bcbde2d429a09a627002fb28085932ec39fb8
Laura Brown <lebrown@mtu.edu>
2023-10-26 16:12:00 -0400

    Update lab9 grader tests

---
+++ lab/week9/tests/q7.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q7',
+    'points': 4,
+    'suites': [   {   'cases': [   {'code': '>>> type(incidence_2021) == float\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(incidence_2021, 2.4, 0.1)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
